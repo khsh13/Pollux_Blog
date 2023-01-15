2e91d9f6f4d8b6083f61c47a7bb6db8584e7e121
Akanksha Jha <100708686+Akanksha-202@users.noreply.github.com>
2023-01-15 19:42:17 +0530

    Add files via upload

---
+++ Pollux/js/blog.js
@@ -0,0 +1,81 @@
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCHiYDxPf_ShRVsEIcc9_edphiPaDaU5q0",
+//   authDomain: "wordpad-46016.firebaseapp.com",
+//   databaseURL: "https://wordpad-46016-default-rtdb.firebaseio.com",
+//   projectId: "wordpad-46016",
+//   storageBucket: "wordpad-46016.appspot.com",
+//   messagingSenderId: "668400414476",
+//   appId: "1:668400414476:web:e6c0af264657227f296329",
+//   measurementId: "G-TCBFR1W0L9"
+// };
+
+// firebase.initializeApp(firebaseConfig);
+
+// //reference from database
+// var blogDB = firebase.database().ref("blogs");
+
+// document.getElementById("blogs").addEventListener("submit", publishBlog);
+
+// function publishBlog(e) {
+//   e.preventDefault();
+
+//   var title = document.getElementById("title");
+//   var content = document.getElementById("content");
+//   console.log("Working");
+
+//   console.log(title, content);
+// }
+
+const firebaseConfig = {
+  //   copy your firebase config informations
+  apiKey: "AIzaSyCHiYDxPf_ShRVsEIcc9_edphiPaDaU5q0",
+  authDomain: "wordpad-46016.firebaseapp.com",
+  databaseURL: "https://wordpad-46016-default-rtdb.firebaseio.com",
+  projectId: "wordpad-46016",
+  storageBucket: "wordpad-46016.appspot.com",
+  messagingSenderId: "668400414476",
+  appId: "1:668400414476:web:e6c0af264657227f296329",
+  measurementId: "G-TCBFR1W0L9"
+};
+
+
+// initialize firebase
+firebase.initializeApp(firebaseConfig);
+
+// reference your database
+var blogDB = firebase.database().ref("blogs");
+
+document.getElementById("blogs").addEventListener("submit", submitForm);
+
+function submitForm(e) {
+  e.preventDefault();
+
+  var title = getElementVal("title");
+  var content = getElementVal("content");
+
+  saveMessages(title, content);
+
+  //   enable alert
+  document.querySelector(".alert").style.display = "block";
+
+  //   remove the alert
+  setTimeout(() => {
+    document.querySelector(".alert").style.display = "none";
+  }, 3000);
+
+  //   reset the form
+  document.getElementById("blogs").reset();
+}
+
+const saveMessages = (title,content) => {
+  var newContactForm = blogDB.push();
+
+  newContactForm.set({
+    title: title,
+    content: content,
+  });
+};
+
+const getElementVal = (id) => {
+  return document.getElementById(id).value;
+};
